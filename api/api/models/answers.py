b3aa46dcbf615e9f83408d28f0b6e2bc79ddf1d7
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2022-12-05 23:04:18 +0530

    Modified the API

--- api/api/models/answers.py
+++ api/api/models/answers.py
@@ -4,30 +4,44 @@ from dataclasses import dataclass
 
 @dataclass
 class Answers(db.Model):
-  id: int
-  answer: str
-  category: str
+  q_category: int
+  answer1: str
+  answer2: str
+  answer3: str
+  answer4: str
+  answer5: str
 
-  id = db.Column(db.Integer, primary_key=True)
-  answer = db.Column(db.String(255), unique=False, nullable=False)
-  category = db.Column(db.Integer)
+  question: str
 
-  def __init__(self, answer, category):
-    self.answer = answer
-    self.category = category
+  q_category = db.Column(db.Integer, primary_key=True)
+  answer1 = db.Column(db.String(255), unique=False, nullable=False)
+  answer2 = db.Column(db.String(255), unique=False, nullable=False)
+  answer3 = db.Column(db.String(255), unique=False, nullable=False)
+  answer4 = db.Column(db.String(255), unique=False, nullable=False)
+  answer5 = db.Column(db.String(255), unique=False, nullable=False)
+  question = db.Column(db.String(255), unique=False, nullable=False)
+
+  def __init__(self, answer1, answer2, answer3, answer4, answer5, question):
+    self.answer1 = answer1
+    self.answer2 = answer2
+    self.answer3 = answer3
+    self.answer4 = answer4
+    self.answer5 = answer5
+
+    self.question = question
 
   def __repr__(self):
-    return str(self.id)
+    return str(self.q_category)
 
 
-def create(answer, category):
-  answerMain = Answers(answer=answer, category=category)
+def create(answer1, answer2, answer3, answer4, answer5, question):
+  answerMain = Answers(answer1=answer1, answer2=answer2, answer3=answer3, answer4=answer4, answer5=answer5, question=question)
   db.session.add(answerMain)
   db.session.commit()
 
 
 def delete(answer_id):
-  answerMain = Answers.query.filter_by(id=answer_id).first()
+  answerMain = Answers.query.filter_by(q_category=answer_id).first()
   if answerMain:
     db.session.delete(answerMain)
     db.session.commit()
